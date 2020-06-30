670902994154b1148b48f1bcb27a7fe9c12d52e6
Peter Kim <peter.kim@bisimulations.com>
2020-06-30 11:55:31 +0900

    replace tab by space.

--- examples/htc_vive_tracker_test.cpp
+++ examples/htc_vive_tracker_test.cpp
@@ -8,110 +8,129 @@ const std::string events_arg = "-e";
 const std::string haptic_arg = "-p";
 const std::string help_arg = "-h";
 
-void ShowDimensionsPlay (const CHtc_Vive_Tracker & vt){
-	Dimension chaperone_dim = vt.GetChaperoneDimensions();
-	std::cout<<"Play area corners: "<<std::endl;
-	PrintVec3(chaperone_dim.corner1);
-	PrintVec3(chaperone_dim.corner2);
-	PrintVec3(chaperone_dim.corner3);
-	PrintVec3(chaperone_dim.corner4);
-	//size in meters
-	std::cout<<"Play area size : "<<std::endl<<"  "<<chaperone_dim.size_x<<" x  "<<chaperone_dim.size_z<<" m"<<std::endl;
+void ShowDimensionsPlay (const CHtc_Vive_Tracker & vt)
+{
+  Dimension chaperone_dim = vt.GetChaperoneDimensions();
+  std::cout<<"Play area corners: "<<std::endl;
+  PrintVec3(chaperone_dim.corner1);
+  PrintVec3(chaperone_dim.corner2);
+  PrintVec3(chaperone_dim.corner3);
+  PrintVec3(chaperone_dim.corner4);
 
-
-
+  //size in meters
+  std::cout<<"Play area size : "<<std::endl<<"  "<<chaperone_dim.size_x<<" x  "<<chaperone_dim.size_z<<" m"<<std::endl;
 }
 
-void Usage (){
-	std::cout<<"Use argument "<<verbose_arg<<" to " <<"see more information" << std::endl;
-	std::cout<<"Use argument "<<events_arg<<" to " <<"loop while checking for events" << std::endl;
-	std::cout<<"Use argument "<<haptic_arg<<" to " <<"trigger a haptic pulse on the tracker and controller after setup" << std::endl;
-	std::cout<<"Use argument "<<help_arg<<" to " <<"see this help" << std::endl;
+void Usage ()
+{
+  std::cout<<"Use argument "<<verbose_arg<<" to " <<"see more information" << std::endl;
+  std::cout<<"Use argument "<<events_arg<<" to " <<"loop while checking for events" << std::endl;
+  std::cout<<"Use argument "<<haptic_arg<<" to " <<"trigger a haptic pulse on the tracker and controller after setup" << std::endl;
+  std::cout<<"Use argument "<<help_arg<<" to " <<"see this help" << std::endl;
 }
 
-void PrintAllDevicesInformation (CHtc_Vive_Tracker & vt){
-	std::cout<<std::endl<<"**************************"<<std::endl;
-	std::vector<std::string> list_of_devices = vt.GetAllDeviceNames();
-	float battery_level = 0 ; 
-	for (unsigned int i = 0; i < list_of_devices.size(); ++i){
-		double pose[3];
-		double quat[4];
-		if (vt.IsDeviceDetected(list_of_devices[i])){
-			bool success = vt.GetDevicePoseQuaternion(list_of_devices[i],pose,quat);
-			Velocity velocity = vt.GetDeviceVelocity (list_of_devices[i]);
-			if (success){
-				std::cout<<list_of_devices[i]<<" Position"<<std::endl;
-				for (int i=0; i<3; ++i) std::cout<<pose[i]<<" ";
-				std::cout<<std::endl;
-				std::cout<<list_of_devices[i]<<" Quaternion"<<std::endl;
-				for (int i=0; i<4; ++i) std::cout<<quat[i]<<" ";
-				std::cout<<std::endl;
-				std::cout<<list_of_devices[i]<<" Linear Velocity"<<std::endl;
-				PrintVec3 (velocity.linear_velocity);
-				std::cout<<list_of_devices[i]<<" Angular Velocity"<<std::endl;
-				PrintVec3 (velocity.linear_velocity);
-				battery_level = vt.GetBatteryLevel (list_of_devices[i]);
-				std::cout<<std::endl<<"Battery level = "<<battery_level<<std::endl;
-				std::cout<<std::endl<<"**************************"<<std::endl;
-				}
-			} else std::cout<<list_of_devices[i]<<" not detected"<<std::endl;
-		}
+void PrintAllDevicesInformation (CHtc_Vive_Tracker & vt)
+{
+  std::cout<<std::endl<<"**************************"<<std::endl;
+  std::vector<std::string> list_of_devices = vt.GetAllDeviceNames();
+  float battery_level = 0 ; 
+  for (unsigned int i = 0; i < list_of_devices.size(); ++i)
+  {
+    double pose[3];
+    double quat[4];
+    if (vt.IsDeviceDetected(list_of_devices[i]))
+    {
+      bool success = vt.GetDevicePoseQuaternion(list_of_devices[i],pose,quat);
+      Velocity velocity = vt.GetDeviceVelocity (list_of_devices[i]);
+      if (success)
+      {
+        std::cout<<list_of_devices[i]<<" Position"<<std::endl;
+        for (int i=0; i<3; ++i) std::cout<<pose[i]<<" ";
+        std::cout<<std::endl;
+        std::cout<<list_of_devices[i]<<" Quaternion"<<std::endl;
+        for (int i=0; i<4; ++i) std::cout<<quat[i]<<" ";
+        std::cout<<std::endl;
+        std::cout<<list_of_devices[i]<<" Linear Velocity"<<std::endl;
+        PrintVec3 (velocity.linear_velocity);
+        std::cout<<list_of_devices[i]<<" Angular Velocity"<<std::endl;
+        PrintVec3 (velocity.linear_velocity);
+        battery_level = vt.GetBatteryLevel (list_of_devices[i]);
+        std::cout<<std::endl<<"Battery level = "<<battery_level<<std::endl;
+        std::cout<<std::endl<<"**************************"<<std::endl;
+      }
+    } 
+    else 
+      std::cout<<list_of_devices[i]<<" not detected"<<std::endl;
+  }
 }
+
 int main(int argc, char *argv[])
 {	
-	bool verbose = false;
-	bool monitor_events = false;
-	bool trigger_haptic_pulse = false;
-	bool display_help = false;
-	for (int i = 1; i < argc; ++i ) {
-		if (argv[i] == verbose_arg) verbose = true;	
-		if (argv[i] == events_arg) monitor_events = true;	
-		if (argv[i] == haptic_arg) trigger_haptic_pulse = true;
-		if (argv[i] == help_arg) display_help = true;
-	}
-	
+  bool verbose = false;
+  bool monitor_events = false;
+  bool trigger_haptic_pulse = false;
+  bool display_help = false;
 
-	if (display_help){
-		Usage();
-		return 0;
-	}
+  for (int i = 1; i < argc; ++i ) 
+  {
+    if (argv[i] == verbose_arg) verbose = true;	
+    if (argv[i] == events_arg) monitor_events = true;	
+    if (argv[i] == haptic_arg) trigger_haptic_pulse = true;
+    if (argv[i] == help_arg) display_help = true;
+  }
 
-	CHtc_Vive_Tracker vt;
+  if (display_help)
+  {
+    Usage();
+    return 0;
+  }
 
-	if (vt.InitializeVR(verbose)){
+  CHtc_Vive_Tracker vt;
 
-		
-        if(vt.GetAllDeviceNames().size()==1){
-            std::cout<<"No devices detected. Check link box are connected and devices are paired"<<std::endl;
-            return 0;
+  if (vt.InitializeVR(verbose))
+  {
+    if(vt.GetAllDeviceNames().size()==1)
+    {
+      std::cout<<"No devices detected. Check link box are connected and devices are paired"<<std::endl;
+      return 0;
+    }
+
+    ShowDimensionsPlay(vt);
+    std::cout<<"Init done."<<std::endl;
+    vt.PrintAllDetectedDevices();
+
+    if (verbose) 
+    {
+      PrintAllDevicesInformation(vt);
+    }
+
+    if (trigger_haptic_pulse)
+    {
+      vt.HapticPulse("tracker_1",0,3999);
+      vt.HapticPulse("controller_1",0,3999);
+    }
+
+    if (monitor_events)
+    {
+        bool button_menu_pressed = false;
+        while (!button_menu_pressed)
+        {
+          if (vt.EventPolling()) 
+          {
+            std::cout<<"Last button pressed : "<<std::endl;
+            std::cout<<vt.GetLastButtonPressedString("tracker_1")<<" ";
+            std::cout<<vt.GetLastButtonPressedString("tracker_2")<<" ";
+            std::cout<<vt.GetLastButtonPressedString("controller_1")<<std::endl;
+            if (vt.GetLastButtonPressedEnum("tracker_1")==vr::k_EButton_ApplicationMenu || 
+                vt.GetLastButtonPressedEnum("controller_1") == vr::k_EButton_ApplicationMenu) 
+              button_menu_pressed = true;
+          }
         }
-		ShowDimensionsPlay(vt);
-		std::cout<<"Init done."<<std::endl;
-		vt.PrintAllDetectedDevices();
-
-		if (verbose) {
-			PrintAllDevicesInformation(vt);
-		}
-		if (trigger_haptic_pulse){
-			vt.HapticPulse("tracker_1",0,3999);
-			vt.HapticPulse("controller_1",0,3999);
-
-		}
-		if (monitor_events){
-		    bool button_menu_pressed = false;
-		    while (!button_menu_pressed){
-			if (vt.EventPolling()) {
-				std::cout<<"Last button pressed : "<<std::endl;
-				std::cout<<vt.GetLastButtonPressedString("tracker_1")<<" ";
-				std::cout<<vt.GetLastButtonPressedString("tracker_2")<<" ";
-				std::cout<<vt.GetLastButtonPressedString("controller_1")<<std::endl;
-				if (vt.GetLastButtonPressedEnum("tracker_1")==vr::k_EButton_ApplicationMenu || vt.GetLastButtonPressedEnum("controller_1") == vr::k_EButton_ApplicationMenu) button_menu_pressed = true;
-			}
-		    }
-		}
-		
-	} else {
-		std::cout<<"Init failed"<<std::endl;
-		if (!verbose) std::cout<<"Try executing with -v for more information"<<std::endl;
-	}
+    }
+  }
+  else 
+  {
+    std::cout<<"Init failed"<<std::endl;
+    if (!verbose) std::cout<<"Try executing with -v for more information"<<std::endl;
+  }
 }
--- htc_vive_tracker.h
+++ htc_vive_tracker.h
@@ -57,116 +57,109 @@ vr::k_EButton_System
 */
 
 
-
-
 void PrintVec3(const Vec3 & v);
 
 class CHtc_Vive_Tracker
-    {
-  private:
-    //IVR System interface (main)
-    vr::IVRSystem * vr_system_;
-    //IVR Chaperone (to get boundaries and size)
-    vr::IVRChaperone * vr_chaperone_;
+{
+private:
+  //IVR System interface (main)
+  vr::IVRSystem * vr_system_;
+  //IVR Chaperone (to get boundaries and size)
+  vr::IVRChaperone * vr_chaperone_;
     
-    //array that contains the positions of all the detected devices
-    vr::TrackedDevicePose_t device_poses_[vr::k_unMaxTrackedDeviceCount];
+  //array that contains the positions of all the detected devices
+  vr::TrackedDevicePose_t device_poses_[vr::k_unMaxTrackedDeviceCount];
 
-    //map to obtain the device id from the device string
-    std::map<std::string,uint32_t> devices_id_;
-    //vector to obtain the device name from the device id
-    std::vector<std::string>devices_names_;
-    //vector to obtain the last pressed button from the device id
-    std::vector<vr::EVRButtonId>last_button_pressed_;
-    // max number of devices
-    uint32_t max_devices_;
-    //Number of devices of each type
-    int hmd_counts_=1, controller_counts_=1, tracker_counts_=1, track_reference_counts_=1, null_counts_=1;
+  //map to obtain the device id from the device string
+  std::map<std::string,uint32_t> devices_id_;
+  //vector to obtain the device name from the device id
+  std::vector<std::string>devices_names_;
+  //vector to obtain the last pressed button from the device id
+  std::vector<vr::EVRButtonId>last_button_pressed_;
+  // max number of devices
+  uint32_t max_devices_;
+  //Number of devices of each type
+  int hmd_counts_=1, controller_counts_=1, tracker_counts_=1, track_reference_counts_=1, null_counts_=1;
     
-    // function to initialize the data structures (devices_names_ and devices_id)
-    void InitializeDeviceMap();
+  // function to initialize the data structures (devices_names_ and devices_id)
+  void InitializeDeviceMap();
     
     
-    EventFlags events_;
+  EventFlags events_;
 
-    //boolean to execute with additional information or not
-    bool verbose_;
+  //boolean to execute with additional information or not
+  bool verbose_;
     
-    //Duration of the haptic pulse
-    const uint32_t MAX_PULSE_DURATION = 3999;
+  //Duration of the haptic pulse
+  const uint32_t MAX_PULSE_DURATION = 3999;
 
-    //Device names
-    static constexpr const char* NAME_HMD = "hmd";
-    static constexpr const char* NAME_CONTROLLER = "controller";
-    static constexpr const char* NAME_TRACKER = "tracker";
-    static constexpr const char* NAME_TREFERENCE = "tracking_reference";
-    static constexpr const char* NAME_NULL = "invalid";
+  //Device names
+  static constexpr const char* NAME_HMD = "hmd";
+  static constexpr const char* NAME_CONTROLLER = "controller";
+  static constexpr const char* NAME_TRACKER = "tracker";
+  static constexpr const char* NAME_TREFERENCE = "tracking_reference";
+  static constexpr const char* NAME_NULL = "invalid";
 
-    //Auxiliary functions to perform transformations
-    //Get the position with Z as the vertical component given a matrix
-    Vec3 MatrixToPoseZVertical(const vr::HmdMatrix34_t & device_matrix);
-    //Get the Quaternion given a matrix
-    void MatrixToQuaternion(const vr::HmdMatrix34_t & device_matrix,double (&angle)[4]);
-    //Update the last pose obtained by a device given its ID
-    bool UpdateDevicePosition(const int device_id);
-
-
-    //Auxiliar functions
-    //Get the class to which the device belongs (tracker, controller, tracking_reference, hmd)
-    std::string GetDeviceClass(const int device_id);
-    //Set the device name by concatenating the class and the index within that class
-    std::string SetDeviceName(const int device_id);
-    //Add new device to the internal list of current active devices
-    bool AddNewDevice(const int device_id);
-    //Delete device from internal list of current active devices
-    bool DeleteDevice(const int device_id);
+  //Auxiliary functions to perform transformations
+  //Get the position with Z as the vertical component given a matrix
+  Vec3 MatrixToPoseZVertical(const vr::HmdMatrix34_t & device_matrix);
+  //Get the Quaternion given a matrix
+  void MatrixToQuaternion(const vr::HmdMatrix34_t & device_matrix,double (&angle)[4]);
+  //Update the last pose obtained by a device given its ID
+  bool UpdateDevicePosition(const int device_id);
 
 
-  public:
-  
-    //Constructor and destructor
-    CHtc_Vive_Tracker();
-    ~CHtc_Vive_Tracker();
+  //Auxiliar functions
+  //Get the class to which the device belongs (tracker, controller, tracking_reference, hmd)
+  std::string GetDeviceClass(const int device_id);
+  //Set the device name by concatenating the class and the index within that class
+  std::string SetDeviceName(const int device_id);
+  //Add new device to the internal list of current active devices
+  bool AddNewDevice(const int device_id);
+  //Delete device from internal list of current active devices
+  bool DeleteDevice(const int device_id);
+
+public:
+  //Constructor and destructor
+  CHtc_Vive_Tracker();
+  ~CHtc_Vive_Tracker();
     
-    //Initialize and shutdown functionalities
-    bool InitializeVR(bool verbose);
-    bool ShutDownVR();
+  //Initialize and shutdown functionalities
+  bool InitializeVR(bool verbose);
+  bool ShutDownVR();
 
-    void Update();
+  void Update();
     
-    //Device detection
-    //Check if device with a given name is detected
-    bool IsDeviceDetected(const std::string & device_name);
-    //Print all currently detected devices
-    void PrintAllDetectedDevices();
-    //Get names of all devicse
-    std::vector<std::string> GetAllDeviceNames();
-    //Get battery level percentage of the device
-    float GetBatteryLevel(const std::string & device_name);
-    //Check for new events
-    bool EventPolling();
+  //Device detection
+  //Check if device with a given name is detected
+  bool IsDeviceDetected(const std::string & device_name);
+  //Print all currently detected devices
+  void PrintAllDetectedDevices();
+  //Get names of all devicse
+  std::vector<std::string> GetAllDeviceNames();
+  //Get battery level percentage of the device
+  float GetBatteryLevel(const std::string & device_name);
+  //Check for new events
+  bool EventPolling();
 
 
-    //Obtain pose (position + quaternion) of a device given its name
-    bool GetDevicePoseQuaternion(const std::string & device_name, double (&pose)[3], double (&angle)[4]);
-    //Obtain pose (position + Euler angles) of a device given its name
-    bool GetDevicePoseEuler(const std::string & device_name, double (&pose)[3], double & roll, double & pitch, double & yaw);
-    //Obtain velocity of a device given its name
-    Velocity GetDeviceVelocity(const std::string & device_name);
+  //Obtain pose (position + quaternion) of a device given its name
+  bool GetDevicePoseQuaternion(const std::string & device_name, double (&pose)[3], double (&angle)[4]);
+  //Obtain pose (position + Euler angles) of a device given its name
+  bool GetDevicePoseEuler(const std::string & device_name, double (&pose)[3], double & roll, double & pitch, double & yaw);
+  //Obtain velocity of a device given its name
+  Velocity GetDeviceVelocity(const std::string & device_name);
 
+  //Chaperone - related functions
+  // Get a vector of the four corners, and the size in X and Z
+  Dimension GetChaperoneDimensions() const;
 
-
-    //Chaperone - related functions
-    // Get a vector of the four corners, and the size in X and Z
-    Dimension GetChaperoneDimensions() const;
-
-    //POGO pin interface - get pressed buttons and send vibrations
-    std::string GetLastButtonPressedString(const std::string & device_name);
-    //Get the last button pressed as obtained from the API
-    vr::EVRButtonId GetLastButtonPressedEnum(const std::string & device_name);
-    // Trigger a haptic pulse on a device given its name
-    bool HapticPulse(const std::string & device_name, uint32_t axis_id, unsigned short duration_microsec);
-
+  //POGO pin interface - get pressed buttons and send vibrations
+  std::string GetLastButtonPressedString(const std::string & device_name);
+  //Get the last button pressed as obtained from the API
+  vr::EVRButtonId GetLastButtonPressedEnum(const std::string & device_name);
+  // Trigger a haptic pulse on a device given its name
+  bool HapticPulse(const std::string & device_name, uint32_t axis_id, unsigned short duration_microsec);
 
 };
 
